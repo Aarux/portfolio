7c0cee49211a3aaa86e2429bb6ea369872323dbe
Aarux <gustav_bergman95@hotmail.com>
2018-11-22 00:11:01 +0100

    Cleaned up code, fixed links to be more visible, finishing touches and edited font sizes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 // Javascript //
 
 // Loading Screen Animation //
-// "Duration" of the animation //
-$("document").ready(function() {
+$('document').ready(function() {
     setTimeout(function(){
         $('body').addClass('loaded');
     }, 3000);
     
 });
-// No JS Backup scroll to top on load/refresh //
-$(document).ready(function(myFunction){
-    $(this).scrollTop(0);
-});
 
-// Safe E-Mail with JQuery/Mailtoo // 
+// "Safe" E-Mail with JQuery Mailtoo // 
 $('a.mail').on('click', function(){
     var href = $(this).attr('href');
     $(this).attr('href', href.replace('badmail.', ''));
@@ -21,28 +16,28 @@ $('a.mail').on('click', function(){
 
 // Smooth Scroll Javascript //
 // Smooth scroll  - Scroll to item 1 //
-$(".scroll1").click(function() {
+$('#nav1').click(function() {
     $('html,body').animate({
-        scrollTop: $(".portfolio-gallery").offset().top -68},
+        scrollTop: $(".projects").offset().top -68},
         'slow');
 });
 
 // Smooth scroll - Scroll to item 2 //
-$(".scroll2").click(function() {
+$('#nav2').click(function() {
     $('html,body').animate({
         scrollTop: $(".about-me").offset().top -68},
         'slow');
 });
 
 // Smooth scroll - Scroll to item 3 //
-$(".scroll3").click(function() {
+$('#nav3').click(function() {
     $('html,body').animate({
         scrollTop: $(".contact-me").offset().top -68},
         'slow');
 });
 
 // Smooth scroll - Scroll to top //
-$(".index").click(function() {
+$('#to-top').click(function() {
     $('html,body').animate({
         scrollTop: 0},
         'slow');
