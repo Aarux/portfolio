d5cf2e2c40d062ffaaeae51936d0f79ffdf69837
Aarux <gustav_bergman95@hotmail.com>
2018-11-22 17:47:46 +0100

    Added media query, finishing touches on code, cleaded up files and minor errors

--- js/main.js
+++ js/main.js
@@ -1,44 +1,50 @@
-// Javascript //
+///////////////////
+//////////////////
+// JavaScript< //
+////////////////
 
-// Loading Screen Animation //
-$('document').ready(function() {
+///////////////////////////////////////
+// Loading Screen Animation Script ///
+$("document").ready(function() {
     setTimeout(function(){
         $('body').addClass('loaded');
     }, 3000);
     
 });
 
-// "Safe" E-Mail with JQuery Mailtoo // 
-$('a.mail').on('click', function(){
-    var href = $(this).attr('href');
-    $(this).attr('href', href.replace('badmail.', ''));
-});
-
-// Smooth Scroll Javascript //
-// Smooth scroll  - Scroll to item 1 //
-$('#nav1').click(function() {
+////////////////////////////////
+// Smooth Scroll Javascript ///
+// Smooth scroll  - Scroll to Item 1 //
+$("#nav1").click(function() {
     $('html,body').animate({
         scrollTop: $(".projects").offset().top -68},
         'slow');
 });
 
-// Smooth scroll - Scroll to item 2 //
-$('#nav2').click(function() {
+////////////////////////////////////////
+// Smooth scroll - Scroll to Item 2 ///
+$("#nav2").click(function() {
     $('html,body').animate({
         scrollTop: $(".about-me").offset().top -68},
         'slow');
 });
 
-// Smooth scroll - Scroll to item 3 //
-$('#nav3').click(function() {
+////////////////////////////////////////
+// Smooth scroll - Scroll to Item 3 ///
+$("#nav3").click(function() {
     $('html,body').animate({
         scrollTop: $(".contact-me").offset().top -68},
         'slow');
 });
 
-// Smooth scroll - Scroll to top //
-$('#to-top').click(function() {
+//////////////////////////////////////////
+// Smooth scroll - Scroll Back to Top ///
+$("#to-top").click(function() {
     $('html,body').animate({
         scrollTop: 0},
         'slow');
 });
+
+////////////////////////////////////////
+/// End of Document ///////////////////
+//////////////////////////////////////
